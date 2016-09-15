6877e9c07d87dc0d721c3622ad7c127b8badde6e
Aleksandar Todorov <alexbig82@gmail.com>
2016-09-15 22:46:12 +0200

    Magic Riven
    EB Addon C#

---
+++ Riven/MagicRiven/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EloBuddy;
+using EloBuddy.SDK.Events;
+using SharpDX;
+
+namespace MagicRiven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Loading.OnLoadingComplete += GameLoaded;
+        }
+
+        private static void GameLoaded(EventArgs args)
+        {
+            Console.WriteLine("Magic Riven");
+        }
+    }
+}
