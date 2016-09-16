1d4bea1b4068b4783996e46f29e63f2bcd30edb5
Aleksandar Todorov <alexbig82@gmail.com>
2016-09-16 21:17:50 +0200

    Core Setup
    Created Core classes and some menu funcs

---
+++ MagicXinZhao/Bases/ChampBase.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+using EloBuddy;
+using EloBuddy.SDK.Menu;
+using EloBuddy.SDK.Events;
+using MagicAIO.Bases.Interface;
+using MagicAIO.Managers;
+
+namespace MagicAIO.Bases
+{
+    abstract class ChampBase : IHero
+    {
+        public static DemageManager demageManager { get; set; }
+
+        public static string CHAMP_MENU { get { return "Magic - " + EloBuddy.Player.Instance.ChampionName; } }
+        // Create Champion Menus
+        protected Menu draw;
+
+        readonly AIHeroClient Player = EloBuddy.Player.Instance;
+
+        protected ChampBase()
+        {
+            CreateMenu();
+
+            draw = GetMenu("Drawings");
+        }
+
+        private Menu GetMenu(string dispayMenuName)
+        {
+            if(MenuManager.Menus[CHAMP_MENU].Keys.Any(it => it == dispayMenuName))
+                return MenuManager.Menus[CHAMP_MENU][dispayMenuName].Keys.Last();
+
+            return null;
+        }
+
+        public virtual void CreateMenu()
+        {
+            MenuManager.Init(CHAMP_MENU);
+        }
+
+        public virtual void TriggerEvents()
+        {
+            Game.OnTick += Game_OnTick;
+        }
+
+        public virtual void CreateVars()
+        {
+
+        }
+
+        public virtual void KillSteal()
+        {
+
+        }
+
+        public virtual void PermaActiv()
+        {
+            KillSteal();
+        }
+
+        public virtual void Game_OnTick(EventArgs args)
+        {
+            if (Player.IsDead)
+                return;
+
+            // Make Kill Stealing Perma Activ
+            PermaActiv();
+        }
+
+    }
+}
---
+++ MagicXinZhao/Managers/MenuManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using EloBuddy.SDK.Menu;
+using EloBuddy.SDK.Menu.Values;
+
+namespace MagicAIO.Managers
+{
+    static class MenuManager
+    {
+        public static Menu CurrentMainMenu;
+        public static Dictionary<string, Dictionary<string, Dictionary<Menu, Dictionary<string, Values>>>> Menus = new Dictionary<string, Dictionary<string, Dictionary<Menu, Dictionary<string, Values>>>>();
+
+        public static Menu Init(string mMenuName)
+        {
+            Menus.Add(mMenuName, new Dictionary<string, Dictionary<Menu, Dictionary<string, Values>>>());
+
+            return CurrentMainMenu = MainMenu.AddMenu(mMenuName, mMenuName);
+        }
+
+        public enum Values
+        {
+            Checkbox = 0, Slider = 1, KeyBind = 2
+        };
+    }
+}
