51f71b285cd6321806897c9a6b48c7fa17471723
Aleksandar Todorov <alexbig82@gmail.com>
2016-09-20 02:00:32 +0200

    AceAIO
    Work In Progress

---
+++ AceAIO/AceAIO/Logics/LogicSelector.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Menu;
+using EloBuddy.SDK.Menu.Values;
+using SharpDX;
+
+using AceAIO.Logics.ChampionLogic;
+using AceAIO.Utilities.MenuManager;
+
+namespace AceAIO.Logics
+{
+    internal class LogicSelector
+    {
+
+        readonly IChampion myChamp;
+        static string MyChamp => Player.Instance.ChampionName;
+
+        public LogicSelector(IChampion my)
+        {
+            myChamp = my;
+            //myChamp.CreateMenu();
+        }
+    }
+}
---
+++ AceAIO/AceAIO/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Reflection;
+using Color = System.Drawing;
+
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Events;
+//using EloBuddy.SDK.Menu;
+//using EloBuddy.SDK.Menu.Values;
+using SharpDX;
+using SharpDX.Direct3D9;
+using Version = System.Version;
+
+using AceAIO.Logics;
+using AceAIO.Logics.ChampionLogic;
+//using AceAIO.Logics.CombatLogic;
+using AceAIO.Utilities;
+//using AceAIO.Utilities.AutoLevelUp;
+using AceAIO.Utilities.MenuManager;
+
+
+namespace AceAIO
+{
+    internal static class Program
+    {
+        //Create Instance of Selected Champion
+        private static AIHeroClient MyHero => Player.Instance;
+        private static IChampion myChampion;
+        private static LogicSelector Logic { get; set; }
+
+        public static void Main()
+        {
+            Loading.OnLoadingComplete += Loading_OnLoadingComplete;
+        }
+
+        private static void Loading_OnLoadingComplete(EventArgs args)
+        {
+            // For Debuging ONLY
+            Chat.Print("Champion Name is: " + MyHero.ChampionName);
+
+           // LogicSelector()
+           
+           
+            
+        }
+    }
+}
