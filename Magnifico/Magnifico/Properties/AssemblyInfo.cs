d8950e90485cbc26dd5cb64b02b94df5c063c3c7
Alex_Power <alexbig82@gmail.com>
2016-09-21 22:07:54 +0200

    Project Structure Established :beer:

--- Magnifico/Magnifico/Properties/AssemblyInfo.cs
+++ Magnifico/Magnifico/Properties/AssemblyInfo.cs
@@ -1,16 +1,34 @@
-﻿using System.Reflection;
-using System.Resources;
+﻿#region License
+// Project:      Magnifico  
+// File Name:    AssemblyInfo.cs
+// 
+// THE WORK (AS DEFINED BELOW) IS PROVIDED UNDER THE TERMS OF THIS CREATIVE COMMONS 
+// PUBLIC LICENSE ("CCPL" OR "LICENSE"). THE WORK IS PROTECTED BY COPYRIGHT AND/OR OTHER 
+// APPLICABLE LAW. ANY USE OF THE WORK OTHER THAN AS AUTHORIZED UNDER THIS LICENSE OR 
+// COPYRIGHT LAW IS PROHIBITED.
+//                                                                                                                                                                                  
+// BY EXERCISING ANY RIGHTS TO THE WORK PROVIDED HERE, YOU ACCEPT AND AGREE TO BE BOUND          
+// BY THE TERMS OF THIS LICENSE. TO THE EXTENT THIS LICENSE MAY BE CONSIDERED TO BE A CONTRACT,    
+// THE LICENSOR GRANTS YOU THE RIGHTS CONTAINED HERE IN CONSIDERATION OF YOUR ACCEPTANCE     
+// OF SUCH TERMS AND CONDITIONS.
+// 
+// 
+// File Created: 2016-09-21
+// Time: 15:10
+//                                                                     Author: Magnifico / Aleksandar Todorov
+#endregion
+using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-
-[assembly: AssemblyTitle("MrWhoAIO")]
-[assembly: AssemblyDescription("All-In-One EloBuddy Addon")]
+[assembly: AssemblyTitle("Magnifico")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MrWhoAIO")]
+[assembly: AssemblyProduct("Magnifico")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -18,12 +36,10 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-
-[assembly: Guid("6ec4489e-6a44-4eb4-8851-f73e27b65ec3")]
+[assembly: Guid("49958a04-4ca4-4b6f-9e17-bfacf927e8f6")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -35,7 +51,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: NeutralResourcesLanguage("en")]
