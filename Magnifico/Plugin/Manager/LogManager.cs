1f0429185d75155cc679b6e500196a22a9d842b3
Alex_Power <alexbig82@gmail.com>
2016-09-22 02:57:34 +0200

    :100:

--- Magnifico/Plugin/Manager/LogManager.cs
+++ Magnifico/Plugin/Manager/LogManager.cs
@@ -1,3 +1,28 @@
+﻿#region License
+// Project:      Magnifico  
+// File Name:    LogManager.cs
+// 
+// THE WORK (AS DEFINED BELOW) IS PROVIDED UNDER THE TERMS OF THIS CREATIVE COMMONS 
+// PUBLIC LICENSE ("CCPL" OR "LICENSE"). THE WORK IS PROTECTED BY COPYRIGHT AND/OR OTHER 
+// APPLICABLE LAW. ANY USE OF THE WORK OTHER THAN AS AUTHORIZED UNDER THIS LICENSE OR 
+// COPYRIGHT LAW IS PROHIBITED.
+//                                                                                                                                                                                  
+// BY EXERCISING ANY RIGHTS TO THE WORK PROVIDED HERE, YOU ACCEPT AND AGREE TO BE BOUND          
+// BY THE TERMS OF THIS LICENSE. TO THE EXTENT THIS LICENSE MAY BE CONSIDERED TO BE A CONTRACT,    
+// THE LICENSOR GRANTS YOU THE RIGHTS CONTAINED HERE IN CONSIDERATION OF YOUR ACCEPTANCE     
+// OF SUCH TERMS AND CONDITIONS.
+// 
+// 
+// File Created: 2016-09-22
+// Time: 02:18
+//                                                                     Author: Magnifico / Aleksandar Todorov
+#endregion
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace Magnifico.Plugin.Manager
 {
     internal class LogManager
